68caeee2f5bced7b8b00e77f92324ce9b7297731
Keilah Marie Scott <keilahscott04@gmail.com>
2026-04-22 08:57:02 -0500

    vue router basic routing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/main.scss'
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).use(router).mount('#app')
